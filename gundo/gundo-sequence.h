1ffcc977fffa12308465233bc5480db07da36c57
Sven Herzberg <sven@imendio.com>
2009-02-09 21:07:49 +0100

    added a header file for the sequence's code
    * gundo/Makefile.am: include the new header file
    * gundo/gundo-sequence.h: added a header file to contain the code for the
      sequence later

---
+++ gundo/gundo-sequence.h
@@ -0,0 +1,29 @@
+/* This file is part of gundo
+ *
+ * AUTHORS
+ *     Sven Herzberg  <herzi@gnome-de.org>
+ *
+ * Copyright (C) 2005,2009  Sven Herzberg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#ifndef GUNDO_SEQUENCE_H
+#define GUNDO_SEQUENCE_H
+
+#include <gundo/gundo.h>
+
+#endif /* !GUNDO_SEQUENCE_H */
