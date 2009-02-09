d03d5fbd026e0ed27d4762bc81781294455e2d07
Sven Herzberg <sven@imendio.com>
2009-02-09 01:14:46 +0100

    added the autotool build system
    * AUTHORS,
    * ChangeLog: added files to satisfy the GNU build type
    * Makefile.am: added the subfolders in an appropriate order
    * autogen.sh: added autogen script for the build system
    * configure.ac: added configure script template
    * src/Makefile: rely on recursive make to properly clean the test folder

---
+++ autogen.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gundo"
+REQUIRED_LIBTOOL_VERSION=1.4.3
+REQUIRED_AUTOMAKE_VERSION=1.7
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "'$srcdir'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+ifs_save="$IFS"; IFS=":"
+for dir in $PATH ; do
+  test -z "$dir" && dir=.
+  if test -f $dir/gnome-autogen.sh ; then
+    gnome_autogen="$dir/gnome-autogen.sh"
+    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
+    break
+  fi
+done
+IFS="$ifs_save"
+
+if test -z "$gnome_autogen" ; then
+  echo "You need to install the gnome-common module and make"
+  echo "sure the gnome-autogen.sh script is in your \$PATH."
+  exit 1
+fi
+
+GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen --disable-update-mimedb --enable-maintainer-mode --enable-gtk-doc $@
+
