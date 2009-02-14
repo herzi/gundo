b9432fb69d27ebaaeedc1ac266ca808ba9b7d849
Sven Herzberg <sven@imendio.com>
2009-02-14 18:49:41 +0100

    added the history to the model
    * gundi-ui/gundo-popup-model.c: added the history to the model

--- gundo-ui/gundo-popup-model.c
+++ gundo-ui/gundo-popup-model.c
@@ -23,6 +23,17 @@
 
 #include "gundo-popup-model.h"
 
+struct _GUndoPopupModelPrivate {
+  GundoHistory* history;
+};
+
+#define PRIV(i) (((GUndoPopupModel*)(i))->_private)
+
+enum {
+  PROP_0,
+  PROP_HISTORY
+};
+
 static void implement_gtk_tree_model (GtkTreeModelIface* iface);
 
 G_DEFINE_TYPE_WITH_CODE (GUndoPopupModel, gundo_popup_model, G_TYPE_OBJECT,
@@ -30,16 +41,71 @@ G_DEFINE_TYPE_WITH_CODE (GUndoPopupModel, gundo_popup_model, G_TYPE_OBJECT,
 
 static void
 gundo_popup_model_init (GUndoPopupModel* self)
-{}
+{
+  PRIV (self) = G_TYPE_INSTANCE_GET_PRIVATE (self, GUNDO_TYPE_POPUP_MODEL, GUndoPopupModelPrivate);
+}
+
+static void
+model_get_property (GObject   * object,
+                    guint       prop_id,
+                    GValue    * value,
+                    GParamSpec* pspec)
+{
+  switch (prop_id)
+    {
+      case PROP_HISTORY:
+        g_value_set_object (value, PRIV (object)->history);
+        break;
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+model_set_property (GObject     * object,
+                    guint         prop_id,
+                    GValue const* value,
+                    GParamSpec  * pspec)
+{
+  switch (prop_id)
+    {
+      case PROP_HISTORY:
+        g_return_if_fail (!PRIV (object)->history);
+
+        PRIV (object)->history = g_value_dup_object (value);
+        g_object_notify (object, "history");
+        break;
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        break;
+    }
+}
 
 static void
 gundo_popup_model_class_init (GUndoPopupModelClass* self_class)
-{}
+{
+  GObjectClass* object_class = G_OBJECT_CLASS (self_class);
+
+  object_class->get_property = model_get_property;
+  object_class->set_property = model_set_property;
+
+  g_object_class_install_property (object_class,
+                                   PROP_HISTORY,
+                                   g_param_spec_object ("history", "history", "history",
+                                                        GUNDO_TYPE_HISTORY,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
+
+  g_type_class_add_private (self_class, sizeof (GUndoPopupModelPrivate));
+}
 
 GtkTreeModel*
 gundo_popup_model_new (GundoHistory* history)
 {
+  g_return_val_if_fail (history, NULL);
+
   return g_object_new (GUNDO_TYPE_POPUP_MODEL,
+                       "history", history,
                        NULL);
 }
 
