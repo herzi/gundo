dc2136e4575d716b2b67786ef42282397d32f8fd
Sven Herzberg <sven@imendio.com>
2009-02-20 23:18:55 +0100

    properly implement the sensitivity
    * gundo-ui/gundo-redo-tool.c: listen for changes to enable/disable the tool
      item

--- gundo-ui/gundo-redo-tool.c
+++ gundo-ui/gundo-redo-tool.c
@@ -27,7 +27,10 @@
 
 #include "gundo-redo-model.h"
 
-G_DEFINE_TYPE (GUndoRedoTool, gundo_redo_tool, GUNDO_TYPE_TOOL);
+static void implement_gundo_history_view (GundoHistoryViewIface* iface);
+
+G_DEFINE_TYPE_WITH_CODE (GUndoRedoTool, gundo_redo_tool, GUNDO_TYPE_TOOL,
+                         G_IMPLEMENT_INTERFACE (GUNDO_TYPE_HISTORY_VIEW, implement_gundo_history_view));
 
 static void
 gundo_redo_tool_init (GUndoRedoTool* self)
@@ -73,5 +76,27 @@ gundo_redo_tool_new (void)
   return g_object_new (GUNDO_TYPE_REDO_TOOL,
                        "stock-id", GTK_STOCK_REDO,
                        NULL);
+}
+
+static gpointer parent_view = NULL;
+
+static void
+tool_notify_can_redo (GundoHistoryView* view,
+                      gboolean          can_redo)
+{
+  gtk_widget_set_sensitive(GTK_WIDGET(view), can_redo);
+
+  if (parent_view && ((GundoHistoryViewIface*)parent_view)->notify_can_redo)
+    {
+      ((GundoHistoryViewIface*)parent_view)->notify_can_undo (view, can_redo);
+    }
+}
+
+static void
+implement_gundo_history_view (GundoHistoryViewIface* iface)
+{
+  parent_view = g_type_interface_peek_parent (iface);
+
+  iface->notify_can_redo = tool_notify_can_redo;
 }
 
